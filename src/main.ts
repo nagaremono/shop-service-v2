c80efc1f956ca58ded859f1a97ecd48a46340e81
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-09-19 01:48:53 +0800

    fix prisma shutdown signal conflict with nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma.service';
 import { AppConfigService } from './shared/providers/AppConfigService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfigService = app.get(AppConfigService);
-  const prismaService = app.get(PrismaService);
-  prismaService.enableShutdownHooks(app);
 
   await app.listen(appConfigService.port);
 }
