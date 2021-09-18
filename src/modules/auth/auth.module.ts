aec374534e667012915da2ab611de39ac32b6e1b
guruhedipurwanto <guruhedipurwanto@gmail.com>
2021-09-19 04:01:06 +0800

    implement logout

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppConfigService } from '../../shared/providers/AppConfigService';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, AppConfigService],
 })
 export class AuthModule {}
--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -1,18 +1,41 @@
-import { BadRequestException } from '@nestjs/common';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Args, Ctx, Mutation, Resolver } from 'type-graphql';
 import { MyContext } from '../../shared/MyContext';
+import { AppConfigService } from '../../shared/providers/AppConfigService';
 import { AuthService } from './auth.service';
 import { AuthMessage } from './dtos/AuthMessage';
 import { LoginArgs } from './dtos/LoginArgs';
 
 @Resolver()
 export class AuthResolver {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private appConfigService: AppConfigService
+  ) {}
 
   @Mutation(() => AuthMessage)
   login(@Args() { email, password }: LoginArgs, @Ctx() { req }: MyContext) {
     return this.authService.login(email, password, req).catch((e) => {
       throw new BadRequestException(e.message);
+    });
+  }
+
+  @Mutation(() => AuthMessage)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+        if (err) {
+          return reject(
+            new InternalServerErrorException('Something went wrong.')
+          );
+        }
+        res.clearCookie(this.appConfigService.cookieName);
+
+        return resolve(AuthMessage.LOGGED_OUT);
+      });
     });
   }
 }
